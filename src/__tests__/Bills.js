5f5c1836f0772d449f08586d6766919b9fe08f18
supmbh <supmbh@gmail.com>
2024-10-05 11:49:46 +0200

    fix test garance début - bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,6 +5,45 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
+import router from "../app/Router.js";
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    // Parcours Employé - Test si l'icône de la page est surlignée
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
+      // À faire : écrire l'expression expect pour vérifier que l'icône est surlignée
+    })
+
+    // Parcours Employé - Test si les notes de frais sont triées correctement
+    test("Then bills should be ordered from earliest to latest", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted) // Le test échoue ici, car la liste reçue n'est pas triée correctement
+    })
+  })
+})
+
+
+/*
+import {screen, waitFor} from "@testing-library/dom"
+import BillsUI from "../views/BillsUI.js"
+import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
@@ -37,3 +76,4 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+*/

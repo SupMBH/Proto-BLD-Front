1dfcc865b0d3e79507043dafe2e52d183b2e2670
supmbh <supmbh@gmail.com>
2024-10-05 17:59:44 +0200

    Ajout test 1 Bills icones sur factures

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -51,44 +51,21 @@ describe("Given I am connected as an employee", () => {
       // Comparaison des dates : attendu vs reçu
       expect(dates).toEqual(datesSorted);
     });
+
+    //AJOUT TEST 1
+    test("Then each bill should have a view details icon", () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const viewIcons = screen.getAllByTestId("icon-eye");
+      expect(viewIcons.length).toBe(bills.length); // Vérifie que toutes les factures ont une icône
+      viewIcons.forEach(icon => {
+        expect(icon).toBeTruthy(); // Vérifie que chaque icône est bien présente
+      });
+    });
+
+
+
+
+
   });
 });
 
-
-/*
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
-
-import router from "../app/Router.js";
-
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
-    })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
-*/
